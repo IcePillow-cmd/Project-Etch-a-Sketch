4e5962a0f937aabd69b80b6608a4d58cbbd1276b
IcePillow <poplizardo4321@gmail.com>
2022-07-29 20:12:17 +0800

    Added function that create cells for grid container

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 const gridCon = document.querySelector("#grid-container");
+
+function createCells(baseNumber) {
+    for (let i = 0; i < Math.pow(baseNumber, 2); i++) {
+        const gridCell = document.createElement("div");
+        gridCell.classList.add("gridCell");
+        gridCell.style.flexBasis = `${100/baseNumber}%`
+        gridCon.appendChild(gridCell);
+    }
+}
+
+createCells(16)
