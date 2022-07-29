e1481a63c4941a6380912f964590c9383f097e04
IcePillow <poplizardo4321@gmail.com>
2022-07-29 20:16:10 +0800

    Changed class that will be added to grid container child

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const gridCon = document.querySelector("#grid-container");
 function createCells(baseNumber) {
     for (let i = 0; i < Math.pow(baseNumber, 2); i++) {
         const gridCell = document.createElement("div");
-        gridCell.classList.add("gridCell");
+        gridCell.classList.add("grid-cell");
         gridCell.style.flexBasis = `${100/baseNumber}%`
         gridCon.appendChild(gridCell);
     }
